b312a2f9a62d4892e21916e8b55b67ad281cffa7
muhib27 <muhib.raven@gmail.com>
2018-04-03 08:52:00 +0600

    adapter

---
+++ app/src/main/java/model/CategoryModel.java
@@ -0,0 +1,116 @@
+package model;
+
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class CategoryModel {
+    //    @SerializedName("code")
+//    @Expose
+//    private String code;
+//
+//    @SerializedName("_embedded")
+//    @Expose
+//    private Embedded embedded;
+
+    @SerializedName("id")
+    @Expose
+    private String id;
+
+    @SerializedName("date")
+    @Expose
+    private String newsDate;
+
+
+    @SerializedName("link")
+    @Expose
+    private String link;
+
+//    @SerializedName("title")
+//    @Expose
+//    private Title title;
+//
+//    @SerializedName("content")
+//    @Expose
+//    private Content content;
+//
+//
+//    @SerializedName("excerpt")
+//    @Expose
+//    private ExcerptModel excerptModel;
+
+//    public CategoryModel(String id, String link, Title title, Content content, ExcerptModel excerptModel) {
+//        this.id = id;
+//        this.link = link;
+//        this.title = title;
+//        this.content = content;
+//        this.excerptModel = excerptModel;
+//    }
+
+    public CategoryModel() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+//    public Title getTitle() {
+//        return title;
+//    }
+//
+//    public void setTitle(Title title) {
+//        this.title = title;
+//    }
+//
+//    public ExcerptModel getExcerptModel() {
+//        return excerptModel;
+//    }
+//
+//    public void setExcerptModel(ExcerptModel excerptModel) {
+//        this.excerptModel = excerptModel;
+//    }
+//
+//    public Content getContent() {
+//        return content;
+//    }
+//
+//    public void setContent(Content content) {
+//        this.content = content;
+//    }
+
+    public String getNewsDate() {
+        return newsDate;
+    }
+
+    public void setNewsDate(String newsDate) {
+        this.newsDate = newsDate;
+    }
+    //    public Object getEmbedded() {
+//        return embedded;
+//    }
+//
+//    public void setEmbedded(Object embedded) {
+//        this.embedded = embedded;
+//    }
+//
+//    public Embedded getEmbedded() {
+//        return embedded;
+//    }
+//
+//    public void setEmbedded(Embedded embedded) {
+//        this.embedded = embedded;
+//    }
+}
