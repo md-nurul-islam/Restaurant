2fde2044758d57bd5d93fbf6670f31d8b22e8f77
Muhib <muhib.raven@gmail.com>
2018-06-26 01:39:48 +0600

    dasas

---
+++ app/src/main/java/com/muhib/restaurant/fcm/AlarmReceiver.java
@@ -0,0 +1,13 @@
+package com.muhib.restaurant.fcm;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class AlarmReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Alarm received!", Toast.LENGTH_LONG).show();
+    }
+}
