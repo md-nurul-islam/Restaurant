d4cbad2710005c857db3925921b8efb82aa6f231
Muhib <muhib.raven@gmail.com>
2018-07-01 08:57:36 +0600

    Printer

--- app/src/main/java/com/muhib/restaurant/activity/LoginActivity.java
+++ app/src/main/java/com/muhib/restaurant/activity/LoginActivity.java
@@ -289,7 +289,7 @@ public class LoginActivity extends AppCompatActivity {
                             finish();
 //
 //                        } else
-                            Toast.makeText(getApplicationContext(),"Success", Toast.LENGTH_SHORT).show();
+                           // Toast.makeText(getApplicationContext(),"Success", Toast.LENGTH_SHORT).show();
                     }
 
 
